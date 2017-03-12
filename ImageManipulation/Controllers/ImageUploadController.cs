4dd6724d5583d065b3d920abdfdbac476408c347
Paresh Chhatrala <paresh.chhatrala@gmail.com>
2017-03-11 20:25:32 -0600

    Add project files.

---
+++ ImageManipulation/Controllers/ImageUploadController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ImageManipulation.Controllers
+{
+    public class ImageUploadController : ApiController
+    {
+        // GET: api/ImageUpload
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/ImageUpload/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/ImageUpload
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/ImageUpload/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/ImageUpload/5
+        public void Delete(int id)
+        {
+        }
+
+        [HttpPost]
+        public int ImageUpload(byte[] byteImageArray)
+        {
+
+
+
+            return 0;
+        }
+    }
+}
---
+++ ImageManipulation/Scripts/Index.js
@@ -0,0 +1,6 @@
+var Greeter = (function () {
+    function Greeter() {
+    }
+    return Greeter;
+}());
+//# sourceMappingURL=Index.js.map
